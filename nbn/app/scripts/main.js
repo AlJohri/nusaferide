e404de883670e8133fb4cb94376236a1a4bcb9f4
duner <asduner@gmail.com>
2015-04-16 02:25:44 -0500

    Fix bower.json

--- nbn/app/scripts/main.js
+++ nbn/app/scripts/main.js
@@ -16,62 +16,59 @@ function getFilteredAndSortedData(data, filter) {
 }
 
 function makeSaferideGraph() {
+  $('#container').highcharts('StockChart', {
 
-    $('#container').highcharts('StockChart', {
+    title: { text: 'Average Montly Saferide Wait Times' },
+    subtitle: { text: 'Data gathered from @adgdsgasd Twitter account' },
 
-        title: { text: 'Average Monthly Saferide Wait Times' },
-        subtitle: { text: 'Data gathered from @adgdsgasd Twitter account' },
+    credits: { enabled: false },
+    // tooltip: { enabled: false },
+    exporting: { enabled: false },
+    rangeSelector: { enabled: false },
+    scrollbar: { enabled: false },
+    navigator: { enabled: false },
 
-        credits: { enabled: false },
-        // tooltip: { enabled: false },
-        exporting: { enabled: false },
-        rangeSelector: { enabled: false },
-        scrollbar: { enabled: false },
-        navigator: { enabled: false },
-
-        plotOptions: {
-          series: {
-            dataGrouping: {
-              approximation: 'average',
-              enabled: true,
-              forced: true,
-              units: [
-                ['month',[1]]
-              ]
-            },
-            animation: false,
-            states: {
-                hover: { enabled: false }
-            }
-          }
+    plotOptions: {
+      series: {
+        dataGrouping: {
+          approximation: 'average',
+          enabled: true,
+          forced: true,
+          units: [
+            ['month',[1]]
+          ]
         },
+        animation: false,
+        states: {
+            hover: { enabled: false }
+        }
+      }
+    },
 
-        series: saferideSeries
+    series: saferideSeries
 
-    });
-
+  });
 }
 
+$(document).ready(function() {
+  $.get('data/graph1.json', function(data) {
 
-$(function() {
-    $.get('data/graph1.json', function(data) {
+    saferideSeries = [
+      {
+        name: '2012-2013',
+        data: getFilteredAndSortedData(data, '2012-2013')
+      }, {
+        name: '2013-2014',
+        data: getFilteredAndSortedData(data, '2013-2014')
+      }, {
+        name: 'Summer 2014',
+        data: getFilteredAndSortedData(data, 'Summer 2014')
+      }, {
+        name: '2014-2015',
+        data: getFilteredAndSortedData(data, '2014-2015')
+      }
+    ];
 
-        saferideSeries = [
-          {
-            name: '2012-2013',
-            data: getFilteredAndSortedData(data, '2012-2013')
-          }, {
-            name: '2013-2014',
-            data: getFilteredAndSortedData(data, '2013-2014')
-          }, {
-            name: 'Summer 2014',
-            data: getFilteredAndSortedData(data, 'Summer 2014')
-          }, {
-            name: '2014-2015',
-            data: getFilteredAndSortedData(data, '2014-2015')
-          }
-        ];
-
-        makeSaferideGraph();
-    });
+    makeSaferideGraph();
+  });
 });
